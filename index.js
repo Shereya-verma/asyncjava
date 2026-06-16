bdedab44334394285d0c919e0e1426980e38a221
Shereya Verma <sv0087218@gmail.com>
2026-06-16 09:38:16 +0530

    changed index.js code

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function sum(n){
-  let ans=0;
-  for(let i=1; i<=n; i++){
-    ans = ans +i;
-
-  }
-  return ans;
-}
-const ans1 = sum(100);
-console.log(ans1);
-const ans2 = sum(1000);
-console.log(ans2);
-const ans3 = sum(10000);
-console.log(ans3);
+const fs = require("fs");
+const contents = fs.readFilesync("a.txt" , "utf-8");
+console.log(contents);
